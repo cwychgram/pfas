1ed393ab219ee7998115dda2357c6eec7705d32c
cwychgram <cwychgr2@jhu.edu>
2025-05-19 15:52:52 -0400

    5-19-25

--- server.R
+++ server.R
@@ -6,62 +6,127 @@ server <- function(input, output, session) {
       "_avg",
       input$year,
       ifelse(input$type == "Population-weighted", "_p", "_a")
-      )
-  })
-
-  output$map <- renderLeaflet({
-    leaflet() %>%
-      addProviderTiles(providers$CartoDB.PositronNoLabels) %>%
-      addResetMapButton() %>%
-      fitBounds(-124.72584, 24.49813, -66.94989, 49.38436)
+    )
   })
   
-  observeEvent(c(input$contaminant, input$type, input$year, input$map_zoom), {
+  output$var <- renderText({
+    myvar()
+  })
+  
+  output$map <- renderLeaflet({
+    leaflet(
+      options = leafletOptions(zoomSnap = 0.25, zoomDelta = 0.25)
+    ) %>%
+      addMapPane(name = "labels", zIndex = 420) %>%
+      addProviderTiles(
+        providers$CartoDB.PositronNoLabels
+      ) %>%
+      addProviderTiles(
+        providers$CartoDB.PositronOnlyLabels,
+        group = "Show Labels",
+        options = leafletOptions(pane = "labels")
+      ) %>%
+      addResetMapButton() %>%
+      setView(-97.000, 38.000, zoom = 4.25) %>%
+      addLayersControl(
+        baseGroups = c(
+          "Hide Labels",
+          "Show Labels"
+        ),
+        options = layersControlOptions(collapsed = FALSE)
+      ) %>%
+      htmlwidgets::onRender("
+    function() {
+        $('.leaflet-control-layers-list').prepend('Basemap');
+    }
+")
+    
+  })
+  
+  observeEvent(c(input$contaminant, input$type, input$year), {
     
     df <- df %>%
       dplyr::select(myvar(), x, y) %>%
       rename(myvar = 1) %>%
       # na.omit() %>%
       mutate(
-        bins = cut(myvar, breaks = c(0, 1, 3, 5, 10, Inf)),
-        cols = colour_values(bins, palette = "ylorbr", na_colour = "#E1E1E1")
+        # bins = cut(myvar, breaks = c(0, 1, 3, 5, 10, Inf)),
+        # cols = colour_values(bins, palette = "ylorbr", na_colour = "#E1E1E1")
+        cols = case_when(
+          # between(round(myvar, 1), 0, .9) ~ "#F7FBFFFF",
+          between(round(myvar, 1), 0, .9) ~ "#E7F0FA",
+          between(round(myvar, 1), 1, 2.9) ~ "#C6DBEFFF",
+          between(round(myvar, 1), 3, 4.9) ~ "#6BAED6FF",
+          between(round(myvar, 1), 5, 9.9) ~ "#2171B5FF",
+          between(round(myvar, 1), 10, Inf) ~ "#08306BFF",
+          TRUE ~ "#E1E1E1"
         )
-
+      )
+    
+    # print(summary(df$myvar[df$cols == "#FFFFE5FF"]))
+    # print(summary(df$myvar[df$cols == "#FEE391FF"]))
+    # print(summary(df$myvar[df$cols == "#FE9929FF"]))
+    # print(summary(df$myvar[df$cols == "#CC4C02FF"]))
+    # print(summary(df$myvar[df$cols == "#662506FF"]))
+    # print(summary(df$myvar[df$cols == "#E1E1E1"]))
+    
     leafletProxy("map") %>%
+      clearGlLayers() %>%
+      clearControls() %>%
       addGlPoints(
         data = st_as_sf(df, coords = c("x", "y"), crs = 4326),
         popup = ~round(myvar, 1),
         fillColor = ~cols,
         fragmentShaderSource = "simpleCircle",
-        radius = input$map_zoom
-      ) 
-  }, ignoreInit = TRUE)
-  
-  observeEvent(c(input$contaminant, input$type, input$year), {
-    leafletProxy("map") %>%
-      clearGlLayers() %>%
-      clearControls() %>%
+        radius = 5
+      ) %>%
       addLegend(
         colors = c(
-          "#FFFFE5FF",
-          "#FEE391FF",
-          "#FE9929FF",
-          "#CC4C02FF",
-          "#662506FF",
+          # "#F7FBFFFF",
+          "#E7F0FA",
+          "#C6DBEFFF",
+          "#6BAED6FF",
+          "#2171B5FF",
+          "#08306BFF",
           "#E1E1E1"
-          ),
+        ),
         labels = c(
-          "0 - 1", 
-          "1 - 3", 
-          "3 - 5",
-          "5 - 10",
-          "10+",
+          "0.0 - 0.9", 
+          "1.0 - 2.9", 
+          "3.0 - 4.9",
+          "5.0 - 9.9",
+          "10.0+",
           "Not tested"
-          ),
+        ),
         opacity = 1,
         title = paste(myvar(), "(ng/L)"),
         position = "bottomright"
       )
+    
+    output$min <- renderText({
+      round(summary(df$myvar)[1], 1)
+    })
+    output$q1 <- renderText({
+      round(summary(df$myvar)[2], 1)
+    })
+    output$med <- renderText({
+      round(summary(df$myvar)[3], 1)
+    })
+    output$mean <- renderText({
+      round(summary(df$myvar)[4], 1)
+    })
+    output$q3 <- renderText({
+      round(summary(df$myvar)[5], 1)
+    })
+    output$max <- renderText({
+      round(summary(df$myvar)[6], 1)
+    })
+    output$n <- renderText({
+      comma(sum(!is.na(df$myvar)))
+    })
+    output$na <- renderText({
+      comma(as.vector(summary(df$myvar)[7]))
+    })
   })
   
 }
--- ui.R
+++ ui.R
@@ -1,6 +1,7 @@
 ui <- page_sidebar(
   title = "Census tract PFAS concentrations",
   sidebar = sidebar(
+    width = "20%",
     selectizeInput(
       inputId = "contaminant",
       label = "Contaminant",
@@ -31,6 +32,27 @@ ui <- page_sidebar(
       value = 2024,
       ticks = FALSE,
       sep = ""
+    ),
+    div(
+      span("Summary"),
+      br(),
+      span("Variable: ", textOutput("var", inline = TRUE)),
+      br(),
+      span("Min: ", textOutput("min", inline = TRUE), "ng/L"),
+      br(),
+      span("Q1: ", textOutput("q1", inline = TRUE), "ng/L"),
+      br(),
+      span("Median: ", textOutput("med", inline = TRUE), "ng/L"),
+      br(),
+      span("Mean: ", textOutput("mean", inline = TRUE), "ng/L"),
+      br(),
+      span("Q3: ", textOutput("q3", inline = TRUE), "ng/L"),
+      br(),
+      span("Max: ", textOutput("max", inline = TRUE), "ng/L"),
+      br(),
+      span("N Available: ", textOutput("n", inline = TRUE), "census tracts"),
+      br(),
+      span("N Missing: ", textOutput("na", inline = TRUE), "census tracts")
     )
   ),
     leafletOutput("map")
